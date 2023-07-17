77a406b5d70b2f249d7e7105c3c022167addab8d
sachindimri788 <sachindimri788@gmail.com>
2023-07-17 08:40:08 +0530

    19

--- routes/indexRoute.js
+++ routes/indexRoute.js
@@ -1,9 +1,10 @@
 const express = require('express');
+const router = express.Router();
 
-const router=require('./userRoute');
+const userRoute=require('./userRoute');
 
 
-router.use('/user',router);
+router.use('/user',userRoute);
 
 module.exports = router;
 
